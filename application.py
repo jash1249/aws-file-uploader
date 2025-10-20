1529ac078d79670631ae9e9c4b59e9e299e35229
Linga Jaswanth <lingajaswanth4545@gmail.com>
2025-10-20 11:02:40 +0530

    Save local changes before pulling

--- application.py
+++ application.py
@@ -71,4 +71,5 @@ def health():
     return jsonify({'status': 'healthy', 'service': 'file-uploader'}), 200
 
 
-
+if __name__ == '__main__':
+    application.run(debug=True, host='0.0.0.0', port=5000)
